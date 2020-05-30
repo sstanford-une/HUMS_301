9dfa942621bc22f4a156c683bff0311dc0f40ac6
Sammy Stanford <1014268@student.sae.edu.au>
2020-05-31 07:13:02 +1000

    Update

--- Coding/HUMS_301/HUMS_301.py
+++ Coding/HUMS_301/HUMS_301.py
@@ -11,32 +11,48 @@ from xml.etree.ElementTree import XMLParser
 from openpyxl import Workbook
 import winsound
 import ntpath
+import numpy as np
+import matplotlib.pyplot as plt
 
-
-global currentTag, currentAttrib, currentData
-global titleData, composerData
 
 
 class FrottalaObject:
-    def __init__(frot, title, composer, text, measures, sections, phrases):
-        frot.title = title
-        frot.composer = composer
-        frot.text = text
-        frot.measures = measures
-        frot.sections = sections
-        frot.phrases = phrases
+    def __init__(frottala, title, composer, sections, phrases):
+        frottala.title = title
+        frottala.composer = composer
+        frottala.sections = sections
+        frottala.phrases = phrases
 
-        print(frot.title, " by ", frot.composer, " has been recorded.")
+        print(frottala.title, "by", frottala.composer, "has been recorded.")
 
-    # return super().__init__(*args, **kwargs)
+class SectionObject:
+    def __init__(section):
+        section.phrases = 0
+        section.measures = 0
+        section.notes = 0
+        section.duration = 0
+        section.syllables = 0
 
+class PhraseObject:
+    def __init__(phrase):
+        phrase.measures = 0
+        phrase.notes = 0
+        phrase.duration = 0
+        phrase.syllables = 0
 
 class DataRecorder:
-    title = None
-    composer = None
-    tag = None
-    attrib = None
-    data = None
+    tag, attrib, data = None, None, None
+
+    def __init__(self):
+        self.title, self.composer = None, None
+        self.sectionObject = SectionObject()
+        self.phraseObject = PhraseObject()
+        self.sections, self.phrases = [], []
+        #self.sectionPhraseCount, self.measureCount, self.noteCount, self.durationCount, self.syllableCount = 0, 0, 0, 0, 0
+        #self.sectionData, self.sectionMeasures, self.sectionSyllables, self.sectionNotes, self.sectionDuration = [], [], [], [], []
+        #self.phraseData, self.phraseMeasures, self.phraseSyllables, self.phraseNotes, self.phraseDuration = [], [], [], [], []
+        self.completeCheck, self.lyricCheck, self.pitchCheck, self.puncCheck, self.capsCheck = False, False, False, False, False
+
     def start(self, tag, attrib):
         self.tag = tag
         self.attrib = attrib
@@ -47,19 +63,13 @@ class DataRecorder:
 
     def data(self, data):
         self.data = data
-        switchFunction = SwitchFunction(self.tag, self.attrib, self.data)
-        switchFunction.FuncSwitch()
+        if self.completeCheck == False:
+            switchFunction = SwitchFunction(self.tag, self.attrib, self.data)
+            switchFunction.FuncSwitch()
         
-        #SwitchFunction.dataFunction()
-        #if self.currentTag == 'credit-words':
-         #   if data != ('Petrucci'):
-          #      self.title = data
-           # elif data == ('Petrucci'):
-            #    self.composer = data
-
     def close(self):
-        print(self.title, 'was composed by', self.composer)
-
+        frottalaObject = FrottalaObject(self.title, self.composer, self.sections, self.phrases)
+        corpusData.append(frottalaObject)
 
 class SwitchFunction:
     def __init__(self, tag, attrib, data):
@@ -69,124 +79,107 @@ class SwitchFunction:
 
     def Info(self):
         if self.data != ('Petrucci'):
-            DataRecorder.title = self.data
+            dataRecorder.title = self.data
         elif self.data == ('Petrucci'):
-            DataRecorder.composer = self.data
+            dataRecorder.composer = self.data
+
+    def MeasureCount(self):
+        if dataRecorder.completeCheck == False:            
+            if self.data == ('dashed'):
+                dataRecorder.sectionObject.measures += 1
+                dataRecorder.phraseObject.measures += 1
+            elif self.data == ('light-heavy'):
+                dataRecorder.sectionObject.measures += 1
+                dataRecorder.phraseObject.measures += 1
+                self.RecordSection()
+                self.RecordPhrase()
+                dataRecorder.completeCheck = True
+            elif self.data != ('dashed') and self.data != ('light-heavy'):
+                dataRecorder.sectionObject.measures += 1
+                dataRecorder.phraseObject.measures += 1
+                self.RecordSection()
+                self.RecordPhrase()
+    
+    def LyricSwitch (self):
+        dataRecorder.lyricCheck = True
+
+    def SyllableCount(self):
+        if dataRecorder.lyricCheck == True:
+            self.CapsCheck()
+            self.PuncSwitch()
+            dataRecorder.sectionObject.syllables += 1
+            dataRecorder.phraseObject.syllables += 1
+            dataRecorder.lyricCheck = False
+
+    def PitchSwitch (self):
+        dataRecorder.pitchCheck = True
+
+    def DurationCount (self):
+        if dataRecorder.pitchCheck == True:
+            dataRecorder.sectionObject.duration += int(self.data)
+            dataRecorder.phraseObject.duration += int(self.data)
+            dataRecorder.pitchCheck = False
+
+    def PuncSwitch(self):
+        punctuation = set(string.punctuation)
+        if any(char in punctuation for char in self.data):
+            dataRecorder.puncCheck = True            
+
+    def CapsCheck(self):
+        if self.data.islower() == False and dataRecorder.puncCheck == True:
+            print(self.data)
+            self.RecordPhrase()
+        else:
+            dataRecorder.capsCheck = False
+            dataRecorder.puncCheck = False
+
 
     def FuncSwitch(self):
         switcher = {
-            'credit-words': self.Info
+            'credit-words': self.Info,
+            'bar-style': self.MeasureCount,
+            'lyric' : self.LyricSwitch,
+            'text' : self.SyllableCount,
+            'pitch' : self.PitchSwitch,
+            'duration' : self.DurationCount
+
             }
         return switcher.get(self.tag, lambda: "Error: Switch Function - FuncSwitch")()
 
-class SectionRecorder:
-    def __init__(section, measureCount, syllableCount, noteCount, phraseCount, barline):
-        section.measureCount = measureCount
-        section.syllableCount = syllableCount
-        section.noteCount = noteCount
+    def RecordSection (self):
+        dataRecorder.sections.append(dataRecorder.sectionObject)
+        dataRecorder.sectionObject = SectionObject()
 
-
-# Declaration of lists used in code
-corpusSyllables = []
-frotSyllables = []
-dataLibrary = []
-syllableDictionary = []
-
-# Declare the corpus used
-projectCorpus = corpus.corpora.LocalCorpus()
-projectCorpus.addPath('E:/Documents/UNE/HUMS_301/XML Files/MusicXML')
-
-# Declare the spreadsheet where data will be recorded
-# workbook = Workbook()
-# sheet = workbook.active
-
-# Collect corpus titles and place into list
-def ProcessTitle():
-    filename = ntpath.basename(file)
-    frotTitle = pieceTitle.replace('.musicxml', '')
-
-def ProcessComposer():
-    for composer in root.iter('creator'):
-        frotComposer = composer.text
-
-
-def ProcessSyllables():
-    for syllable in root.iter('text'):
-        corpusSyllables.append(syllable.text)
-        frotSyllables.append(syllable.text)
-
-
-def CreateDictionary(inputList):
-    return list(dict.fromkeys(inputList))
-
-# def RecordSection():
-
-def CountMeasures():
-    measureCount = 0
-    sectionMeasureCount = 0
-
-    frotMeasureData = [0]
-    frotSectionData = []
-    for measure in root.iter('measure'):
-        if int(measure.get('number')) > measureCount:
-            measureCount = int(measure.get('number'))
-    frotMeasureData.append(frotBarCount)
-
-    for barline in root.iter('bar-style'):
-        sectionData = []
-        if barline.text == 'dashed':
-            measureCount += 1
-        elif barline.text != 'dashed' and barline.text != 'light-heavy':
-            sectionData = [sectionBarCount, barline.text]
-            frotSectionData.append(sectionData)
-        elif barline.text == 'light-heavy':
-            sectionData = [sectionBarCount, barline.text]
-            frotSectionData.append(sectionData)
-            break
+    def RecordPhrase (self):
+        dataRecorder.phrases.append(dataRecorder.phraseObject)
+        dataRecorder.phraseObject = PhraseObject()
+        dataRecorder.lyricCheck, dataRecorder.pitchCheck, dataRecorder.puncCheck, dataRecorder.capsCheck = False, False, False, False
     
-        section = SectionRecorder()
-
-    frotData.append(frotMeasureData)
-    
-
 
 def ProcessCorpus():
     print("Gathering Files")
-    global root, frotData, frotTitle, frotComposer, frotMeasures, frotPhrases
-    
+    global root, corpusData, dataRecorder, corpCount
+    corpusData = []
+
     for file in glob(r'E:/Documents/UNE/HUMS_301/XML Files/MusicXML\*.musicxml'):
         tree = etree.parse(file)
         root = tree.getroot()
         xmlString = etree.tostring(root, encoding="UTF-8", method='xml')
-        # frotData[name, composer, measures]
-        frotData = []
-        frottalaData = DataRecorder()
-        parser = XMLParser(target = frottalaData)
+        dataRecorder = DataRecorder()
+        parser = XMLParser(target = dataRecorder)
         parser.feed(xmlString)
         parser.close()
-        #ProcessTitle()
-        #ProcessComposer()
-        #CountMeasures()
-        #ProcessSyllables()
-        #dataLibrary.append(frotData)
+
+          
+    # for frottala in somewhere longest data = column amount
+    # put in excel using biggest values
 
     print('Corpus procssing compelete!')
 
-def ProgressReport():
-	global progress
-	progress += 1
-	print("Files Processed: ", progress, "/62")
 
-def ProgressReset():
-	global progress
-	progress = 0
 
-# Initate functions in sequence
+
 ProcessCorpus()
-# print(*corpusTitles, sep = ", ")
-syllableDictionary = CreateDictionary(corpusSyllables)
-# print(*syllableDictionary, sep = ", ")
-print(len(dataLibrary))
-# print(len(frotData[0][2][1]))
-#x = FrottalaObject('Poop', [1, 2, 3])
+
+
 
